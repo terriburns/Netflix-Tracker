42b16aee2de267e279de448bd5e378b2baa05097
Terri Burns <terri.burns@nyu.edu>
2016-05-07 17:22:23 -0400

    Mongo error checking

--- db.js
+++ db.js
@@ -2,25 +2,35 @@ var mongoose = require('mongoose');
 var passportLocalMongoose = require('passport-local-mongoose');
 
 var Show = new mongoose.Schema({
-  title: String,
-  totalSeasons: Number,
-  totalEpisodes: Number,
-  epLength: Number,
+  title: {
+    type:String, 
+    required:[true, 'Show title is required!']
+  },
+  totalSeasons: {
+    type:Number,  
+    min:[1, 'The total number of seasons must be greater than 0'],
+    required:[true, 'The total number of seasons is required'],
+  },
+  totalEpisodes: {
+    type:Number, 
+    min:[1, 'The total number of episodes must be greater than 0'],
+    required:[true, 'The total number of episodes is required'],
+  },
+  epLength: {
+    type:Number, 
+    min:[1, 'The episode length must be greater than 0'],
+    required:[true, 'The episode length is required'],
+  },
   netflixPercentage: Number,
   lifePercentage: Number
 });
 
 var User = new mongoose.Schema({
-  username: String,
+  username: {type:String, required:[true, 'Username is required!']},
   password: String,
-  birthday: String,
+  birthday: {type:String, required:[true, 'Birthday is required!']},
   shows: [Show],
   userTotal: Number
-});
-
-
-var Total = new mongoose.Schema({
-  totalValue: Number
 });
 
 User.plugin(passportLocalMongoose);

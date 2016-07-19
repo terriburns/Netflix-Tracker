4d2e2a26d9c4a540fd8038dcbaa1323114ecc693
Terri Burns <terri.burns@nyu.edu>
2016-07-18 20:59:31 -0700

    Update to deploy to heroku

--- app.js
+++ app.js
@@ -80,5 +80,6 @@ app.use(function(err, req, res, next) {
 });
 
 //serve it up
-app.listen(3000);
-console.log("Server started on port 3000");
+var portNum = Number(process.env.PORT || 3000);
+app.listen(portNum);
+console.log("Server started on port process.env.PORT or 3000");
